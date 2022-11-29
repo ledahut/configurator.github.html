45697ef631943080f943568b547ded63522c9353
ledahut <clem.bianchi@gmail.com>
2022-11-29 13:13:33 +0100

    Configurator

---
+++ keyshot-configurator-imagemapping.js
@@ -0,0 +1,15 @@
+imagemappingdata =
+[
+    {
+        "image": "untitled.160.1.jpg",
+        "modelsets": [
+            1828581376
+        ]
+    },
+    {
+        "image": "untitled.160.2.jpg",
+        "modelsets": [
+            1828579748
+        ]
+    }
+];
---
+++ keyshot-configurator-modeldata.js
@@ -0,0 +1,18 @@
+model =
+{
+    "materialgroups": [],
+    "products": [
+        {
+            "checked": true,
+            "icon": "3.png",
+            "modelset": 1828581376,
+            "name": "3"
+        },
+        {
+            "icon": "2.png",
+            "modelset": 1828579748,
+            "name": "2"
+        }
+    ],
+    "studios": []
+};
